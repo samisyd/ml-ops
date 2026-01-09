2046c0ab77a87b248dd75400abb306d66f0d7189
samisyd <111076416+samisyd@users.noreply.github.com>
2026-01-09 15:42:29 -0800

    UserInterface using FastApi's to test the model

---
+++ main.py
@@ -0,0 +1,26 @@
+# main.py
+from fastapi import FastAPI
+from pydantic import BaseModel
+import numpy as np
+import joblib
+
+
+app = FastAPI()
+model = joblib.load("diabetes_model.pkl")
+
+class DiabetesInput(BaseModel):
+    Pregnancies: int
+    Glucose: float
+    BloodPressure: float
+    BMI: float
+    Age: int
+
+@app.get("/")
+def read_root():
+    return {"message": "Diabetes Prediction API"}
+
+@app.post("/predict")
+def predict(data: DiabetesInput):
+    input_data = np.array([[data.Pregnancies, data.Glucose, data.BloodPressure, data.BMI, data.Age]])
+    prediction = model.predict(input_data)[0]
+    return {"diabetic": bool(prediction)}
